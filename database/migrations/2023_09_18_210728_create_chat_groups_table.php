c29227f5317b016e1bb06bd510146ed2253acd27
kgrymd <kgroymd@gmail.com>
2023-09-27 21:26:16 +0900

    add month and year columns to chat_groups table

--- database/migrations/2023_09_18_210728_create_chat_groups_table.php
+++ database/migrations/2023_09_18_210728_create_chat_groups_table.php
@@ -16,8 +16,9 @@ return new class extends Migration
             $table->uuid('uuid')->unique();
             $table->string('name', 30)->nullable();
             $table->foreignId('recruitment_id')->nullable()->constrained(); // 追加2023/09/19
+            $table->unsignedSmallInteger('month')->nullable(); // 月を表すカラムの追加
+            $table->unsignedSmallInteger('year')->nullable(); // 年を表すカラムの追加
             $table->timestamps();
-
             $table->index('uuid');
         });
     }
